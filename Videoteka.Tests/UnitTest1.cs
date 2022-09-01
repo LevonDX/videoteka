b3ee0da0f4ed3acc1d7373cd5072f0d7bb6e8973
Levon Hovhannisyan <lehovhan@microsoft.com>
2022-09-01 19:23:48 +0400

    Initial commit

---
+++ Videoteka.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Videoteka.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
